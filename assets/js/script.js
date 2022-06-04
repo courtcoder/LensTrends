fbf19db393728842f6805c38b920c186131e98fb
courtcoder <courtney1187@icloud.com>
2022-06-04 18:03:14 -0400

    added search function and checkboxes to modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,68 +5,111 @@ var continueBtnEl = document.querySelector("#continue-btn");
 var inputBarEl = document.querySelector("#input-bar");
 var submitBntEl = document.querySelector("#submit-name");
 
+//variables
+var photosContainer = document.getElementById("photos-container");
+var formContainer = document.getElementById("form-container");
+
+//list all category options for checkbox/buttons
+var catsOptions = ["music", "animals", "sports", "movies"];
+
+// // search button
+const searchbtn = document.querySelector(".searchbtn");
+
+
+// get answers Array from local storage
+var pastUsers = [];
+var newUser;
+var LSpastUsers = JSON.parse(localStorage.getItem("pastUsers"));
+if (LSpastUsers) {
+  pastUsers = LSpastUsers;
+  newUser = false;
+} else {
+  pastUsers = [];
+  newUser = true;
+}
+var userName;
+var startCats = [];
+
 // will show modal
 window.onload = function () {
   introModalEl.classList = "display";
   mainPageEl.classList = "noDisplay";
+  modalCheckboxes();
+  if (newUser) {
+  } else {
+    modalPopulate();
+  }
 };
 
-// will make modal disapear and show main page
-function showMainPage() {
-  introModalEl.classList = "noDisplay";
-  mainPageEl.classList = "display";
-}
+var modalCheckboxes = function () {
+  for (var c = 0; c < catsOptions.length; c++) {
+    cc = c + 1;
+    console.log(cc);
+    var inputLine = document.createElement("div");
+    var checkbox = document.createElement("input");
+    var labelCheckbox = document.createElement("label");
+    var linebreak = document.createElement("br");
+    checkbox.setAttribute("name", "preference" + cc);
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("id", catsOptions[c]);
+    checkbox.setAttribute("value", catsOptions[c]);
+    //or can add classes for styling it
+    // inputLine.setAttribute("style", "display:inline-flex");
+    labelCheckbox.setAttribute("for", catsOptions[c]);
+    labelCheckbox.textContent = catsOptions[c];
 
-function nameInput() {
-  const setName = inputBarEl.value;
-  localStorage.setItem("USER", JSON.stringify(setName));
-  console.log(setName);
-}
+    inputLine.appendChild(checkbox);
+    inputLine.appendChild(labelCheckbox);
+    formContainer.appendChild(inputLine);
+    formContainer.appendChild(linebreak);
+  }
+};
 
-function pullImages() {
-  // ACCESS TO URL
-  var queryParams = "?query=" + category;
-  var finalURL = url + queryParams;
-  console.log(finalURL);
-  fetch(finalURL, {
-    headers: {
-      Authorization: API_KEY,
-    },
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      // data.photos
-      photosContainer.innerHTML = "";
-      for (let i = 0; i < data.photos.length; i++) {
-        const photo = data.photos[i];
-        console.log(photo);
-        var imageElement = document.createElement("img");
-        imageElement.src = photo.src.small;
-        imageElement.classList.add("photo");
-        console.log(photo.src.original);
-        photosContainer.appendChild(imageElement);
-      }
-    });
-}
-var musicCatBtn = document.querySelector("#music-btn");
-musicCatBtn.addEventListener("click", setCat);
-var booksCatBtn = document.querySelector("#books-btn");
-booksCatBtn.addEventListener("click", setCat);
-var travelCatBtn = document.querySelector("#travel-btn");
-travelCatBtn.addEventListener("click", setCat);
-var sportsCatBtn = document.querySelector("#sports-btn");
-sportsCatBtn = addEventListener("click", setCat);
-var natureCatBtn = document.querySelector("#nature-btn");
-natureCatBtn = addEventListener("click", setCat);
-var photosContainer = document.querySelector("#photos-container");
+var modalPopulate = function () {
+  // say welcome userName
+  //populate radio buttons
+  // change title
+  console.log(pastUsers);
+  document.getElementById("nameTitle").innerHTML =
+    "Welcome back " + pastUsers[0];
+  document.querySelector("#name-bar").innerHTML = "";
+  for (var a = 1; a < pastUsers.length; a++) {
+    var answerFill = document.querySelector(
+      'input[name="preference' + a + '"]'
+    );
+    console.log(answerFill);
+    if (pastUsers[a] === true) {
+      answerFill.checked = true;
+    }
+  }
+  //add a I'm not pastUsers[0] bar to clear ls and reload
+};
 
-function setCat(evt) {
-  category = evt.target.value;
-  pullImages();
-}
+function gatherInput() {
+  //grab this
+  if (newUser) {
+    const setName = inputBarEl.value;
+    pastUsers[0] = setName;
+  }
+  //get checkbox answers //if checked then true, else false
 
-continueBtnEl.addEventListener("click", showMainPage);
-submitBntEl.addEventListener("click", nameInput);
+  for (var c = 0; c < catsOptions.length; c++) {
+    cc = c + 1;
+    console.log(cc);
+    var checkedTF = document.querySelector(
+      '[name="preference' + cc + '"]'
+    ).checked;
+    console.log(checkedTF.checked);
+    if (checkedTF) {
+      startCats.push(catsOptions[c]);
+      pastUsers[cc] = true;
+      console.log(startCats);
+    } else {
+      pastUsers[cc] = false;
+    }
+  }
+
+  localStorage.setItem("pastUsers", JSON.stringify(pastUsers));
+  // console.log(setName);
+  //get categories
+}
